dadcd726972e28013e0cf83273403cbbe67000d4
hroxo <hugo.roxo04@gmail.com>
2025-09-11 00:26:35 +0100

    added new puts to the helper func

--- ft_printf/srcs/helper.c
+++ ft_printf/srcs/helper.c
@@ -6,21 +6,76 @@
 /*   By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 12:14:01 by hroxo             #+#    #+#             */
-/*   Updated: 2025/09/10 12:30:45 by hroxo            ###   ########.fr       */
+/*   Updated: 2025/09/11 00:23:42 by hroxo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include "header.h"
+#include <unistd.h>
 
-size_t	print_arg(va_list args, char type)
+size_t	ft_strlen(char *str)
 {
-	size_t	printed_char;
+	size_t	i;
 
-	if (type == "char")
-	{
-		ft_putstr_fd(args);
-	}
-	return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
+
+size_t	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+size_t	ft_putstr(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_putnbr_base(int nb, char *base)
+{
+	size_t	printed;
+	long	nbl;
+	char	c;
+	int		len;
+
+	printed = 0;
+	nbl = nb;
+	len = ft_strlen(base);
+	if (nbl < 0)
+	{
+		ft_putchar('-');
+		nbl *= -1;
+	}
+	if (nbl > len)
+	{
+		printed += ft_putnbr_base(nbl / len, base);
+	}
+	c = base[nbl % len];
+	printed += ft_putchar(c);
+	return (printed);
+}
+/*
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	size_t	size;
+
+	printf("value\n");
+	size = ft_putnbr_base(atoi(argv[1]), argv[2]);
+	printf("\n");
+	printf("return value: %d\n", (int)size);
+	return 0;
+}
+*/
--- ft_printf/util/header.h
+++ ft_printf/util/header.h
@@ -6,18 +6,21 @@
 /*   By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 22:56:45 by hroxo             #+#    #+#             */
-/*   Updated: 2025/09/10 12:30:46 by hroxo            ###   ########.fr       */
+/*   Updated: 2025/09/11 00:25:36 by hroxo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
 
-#include <stdlib.h>
-#include <stdarg.h>
-#include "libft.h"
+# include <stdlib.h>
+# include <stdarg.h>
 
 size_t	print_arg(va_list args, char *type);
+size_t	ft_strlen(char *str);
 size_t	count_args(const char *str);
+size_t	ft_putchar(char c);
+size_t	ft_putstr(char *str);
+size_t	ft_putnbr_base(int nb, char *base);
 
 #endif
